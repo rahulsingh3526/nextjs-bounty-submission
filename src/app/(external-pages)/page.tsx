6d9076e746ad8e1fd1d20b4869796bcae83265f3
Rahul_Singh <koderrahul@gmail.com>
2024-06-22 19:02:13 +0530

    feat: bento grid

--- src/app/(external-pages)/page.tsx
+++ src/app/(external-pages)/page.tsx
@@ -4,35 +4,32 @@ import { Button } from '@/components/ui/button';
 import Image from 'next/image';
 import badgeicon1 from '../../../public/icons/SVG_margin.png';
 import badgeicon2 from '../../../public/icons/SVG.png';
+import quote from '../../../public/icons/SVG (1).png';
 import landingPageImage from '../../../public/image 8.png';
 import { AnimatedSubscribeButton } from '@/components/magicui/animated-subscribe-button';
 import { CheckIcon, ChevronRightIcon } from 'lucide-react';
 // import { AnimatedShinyTextDemo } from '@/components/badge-component';
 import { MarqueeDemo } from '@/components/marquee-component';
 import { BentoDemo } from '@/components/bento-component';
+import { OrbitingCirclesDemo } from '@/components/orbiting-circles';
+import { DefaultTextComponent } from '@/components/default-text-component';
+import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
+import { Separator } from '@/components/ui/separator';
 
 const LandingPage = () => {
   return (
     <div>
       <div className=" flex flex-col items-center justify-center gap-y-2 m-10">
         {' '}
-        <div className="flex justify-center items-center">
-          {' '}
-          <Badge variant="secondary" className="rounded-full gap-x-1">
-            <Image src={badgeicon1} alt="badge icons" />
-            <p> Introduction</p>
-            <Image src={badgeicon2} alt="badge icons" />
-          </Badge>
-        </div>
-        <h1 className="text-3xl font-semibold">
-          {' '}
-          NextBase Ultimate Landing Page
-        </h1>
-        <p className="text-[#64748B] text-center">
-          Welcome to Nextbase Ultimate Landing Page: Your Gateway to Innovation,
-          Discover The Future of <br />
-          Excellence and Elevate Your Experience
-        </p>
+        <DefaultTextComponent
+          image1={badgeicon1}
+          image2={badgeicon2}
+          text1="Introduction"
+          text2="NextBase Ultimate Landing Page"
+          text3="Welcome to Nextbase Ultimate Landing Page: Your Gateway to Innovation,
+          Discover The Future of"
+          text4="Excellence and Elevate Your Experience"
+        />
         <div className="flex ">
           <Button variant="default" className="w-40">
             Log In
@@ -63,37 +60,77 @@ const LandingPage = () => {
           className="max-w-[1104px]"
         />
       </div>
-
-      <div>
+      <div className="text-center">
         <p>Trusted by 150+ companies</p>
       </div>
       <div>
         {/* <AnimatedShinyTextDemo /> */}
         <div className=" flex flex-col items-center justify-center gap-y-2 m-10">
           {' '}
-          <div className="flex justify-center items-center">
-            {' '}
-            <Badge variant="secondary" className="rounded-full gap-x-1">
-              <Image src={badgeicon1} alt="badge icons" />
-              <p> Introduction</p>
-              <Image src={badgeicon2} alt="badge icons" />
-            </Badge>
-          </div>
-          <h1 className="text-3xl font-semibold">
-            {' '}
-            NextBase Ultimate Landing Page
-          </h1>
-          <p className="text-[#64748B] text-center">
-            Welcome to Nextbase Ultimate Landing Page: Your Gateway to
-            Innovation, Discover The Future of <br />
-            Excellence and Elevate Your Experience
-          </p>
+          <DefaultTextComponent
+            image1={badgeicon1}
+            image2={badgeicon2}
+            text1="Features"
+            text2="Discover Next-Level Features"
+            text3="Discover the ultimate insights into cutting-edge advancements.Our next-level"
+            text4="Excellence and Elevate Your Experience"
+          />
         </div>
       </div>
-      <MarqueeDemo />
       <div className="flex justify-center items-center mx-auto max-w-[1104px]">
         {' '}
         <BentoDemo />
+      </div>
+      <div className="flex flex-col justify-center items-center gap-y-4">
+        <Image src={quote} alt="quote" />
+        <h1 className="text-3xl">
+          lorem ipsum dolor sit amet, consecterur adpiscing elit integer nex
+          odio prasenet libero
+        </h1>
+
+        <div className="flex justify-center items-center gap-x-4">
+          {' '}
+          <Avatar>
+            <AvatarImage src="https://github.com/shadcn.png" />
+            <AvatarFallback>CN</AvatarFallback>
+          </Avatar>
+          <p className="semi-bold">Mark Zuckerberg</p>
+          <Separator
+            orientation="vertical"
+            className="h-7 w-[3px] bg-neutral-500 "
+          />
+          <p>CEO, Facebook</p>
+        </div>
+      </div>
+
+      <DefaultTextComponent
+        image1={badgeicon1}
+        image2={badgeicon2}
+        text1="Integration"
+        text2="Master Integration Like Pros"
+        text3="lorem ipsum dolor sit amet, consecterur adpiscing elit "
+        text4="integer nex
+          odio prasenet libero"
+      />
+
+      <div className="flex justify-center items-center mx-auto max-w-[1104px]">
+        {' '}
+        <OrbitingCirclesDemo />
+      </div>
+      <MarqueeDemo />
+
+      <div className=" flex flex-col items-center justify-center gap-y-2 m-10">
+        {' '}
+        <Image src={badgeicon1} alt="badge icons" />
+        <h1 className="text-3xl font-semibold">
+          {' '}
+          Discover Next-Level Features
+        </h1>
+        <p className="text-[#64748B] text-center">
+          Discover the ultiimate insights into cutting-edge advancements.Our
+          next-level <br />
+          features guide reveals the essentials for staying ahead.
+        </p>
       </div>
     </div>
   );
---
+++ src/components/default-text-component.tsx
@@ -0,0 +1,51 @@
+import React from 'react';
+import { Badge } from './ui/badge';
+import Image, { StaticImageData, StaticImageData } from 'next/image';
+
+// const data = [
+//   {
+//     text1: 'NextBase Ultimate Landing Page',
+//     text2:
+//       'Welcome to Nextbase Ultimate Landing Page: Your Gateway to Innovation, Discover The Future of',
+//     text3: 'Excellence and Elevate Your Experience',
+//   },
+// ];
+
+interface Props {
+  image1: StaticImageData;
+  image2: StaticImageData;
+  text1: string;
+  text2: string;
+  text3: string;
+  text4: string;
+}
+
+export const DefaultTextComponent = ({
+  image1,
+  image2,
+  text1,
+  text2,
+  text3,
+  text4,
+}: Props) => {
+  return (
+    <div>
+      <div className="flex flex-col items-center justify-center gap-y-2 m-10">
+        <div className="flex justify-center items-center">
+          {' '}
+          <Badge variant="secondary" className="rounded-full gap-x-1">
+            <Image src={image1} alt="badge icons" />
+            <p> {text1}</p>
+            <Image src={image2} alt="badge icons" />
+          </Badge>
+        </div>
+        <h1 className="text-3xl font-semibold"> {text2}</h1>
+        <p className="text-[#64748B] text-center">
+          <span>{text3} </span>
+          <br />
+          <span>{text4}</span>
+        </p>
+      </div>
+    </div>
+  );
+};
